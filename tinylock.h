1aa3c874e05ae311559a195c833e9c356f1a4733
Gavrila Alexandru-Valentin <108356192+gavrilaalexandru@users.noreply.github.com>
2024-11-02 17:50:49 +0200

    Update tinylock.h

--- tinylock.h
+++ tinylock.h
@@ -1,12 +1,19 @@
 #pragma once
 #include <string>
-#include <thread>
-#include <chrono>
-#include <vector>
-#include <ctime>
-#include <windows.h>
-#include <algorithm>
-#include <random>
 
+void set_console_title(const std::string& title);
+void display_ascii_art();
+void display_menu();
+void display_action_menu();
+void post_action_menu();
 std::string caesar_encrypt(std::string text, int shifting_value);
 std::string caesar_decrypt(std::string text, int shifting_value);
+std::string clean_key(const std::string& key);
+std::string vigenere_encrypt(std::string plaintext, std::string key);
+std::string vigenere_decrypt(std::string plaintext, std::string key);
+std::string handle_caesar_encrypt();
+std::string handle_caesar_decrypt();
+std::string handle_vigenere_encrypt();
+std::string handle_vigenere_decrypt();
+void main_handle_caesar_chiper();
+void main_handle_vigenere_chiper();
